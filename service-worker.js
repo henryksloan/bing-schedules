26cc6ece93e057b3cfd32844551b975abfcbea5e
Henry Sloan <henryksloan@gmail.com>
2018-10-19 12:50:02 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/bing-schedules/precache-manifest.52903dfc2380c66939a0683582abf5e0.js"
+  "/bing-schedules/precache-manifest.cc9c3607e6655c7275e03405c13fc753.js"
 );
 
 workbox.clientsClaim();
