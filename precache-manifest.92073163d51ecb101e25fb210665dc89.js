4f1ccccfb5cc06ec24a89aad0587df7cbecefafe
Henry Sloan <henryksloan@gmail.com>
2019-10-04 13:34:18 -0400

    Updates

---
+++ precache-manifest.92073163d51ecb101e25fb210665dc89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10941b579ab7e954b1a81535c0b9b91b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54b1e7bda474e6685b67",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6955133ccf42e670d241",
+    "url": "/static/css/main.0c29f14a.chunk.css"
+  },
+  {
+    "revision": "54b1e7bda474e6685b67",
+    "url": "/static/js/2.3d3590ea.chunk.js"
+  },
+  {
+    "revision": "51dd5a80f8c2bfb3cb1a",
+    "url": "/static/js/3.f89ed6c9.chunk.js"
+  },
+  {
+    "revision": "6955133ccf42e670d241",
+    "url": "/static/js/main.eaf766e7.chunk.js"
+  },
+  {
+    "revision": "c2d3dba7033e5a5d0aed",
+    "url": "/static/js/runtime-main.d03439e2.js"
+  }
+]);
